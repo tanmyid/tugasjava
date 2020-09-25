a227f3ee1f89bad32a60fbf4159e9c1f5c85ca1c
tanmyid <tanmyid1337@gmail.com>
2020-09-25 23:14:53 +0000

    Tugas Java Bu Nanik

---
+++ Data_Mahasiswa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HaloUMK;
+
+/**
+ *
+ * @author Tanio
+ * yang dapat saya jelaskan dari program sederhana tersebut adalah
+ * java merupakan bahasa pemprograman yang case sensitive dimana huruf kecil dan huruf kapital di artikan berbeda, contohnya seperti metodh main line pertama
+ * setiap menulis bahasa pempropgraman harus teliti, contohnya seperti metodh main line ke empat
+ */
+public class Data_Mahasiswa {
+    public static void main(String args[])
+{
+// yang salah : system.out.println("Nama : Tuliskan Nama Anda"); pembenaran di bawahnya
+System.out.println("Nama : Mohammad Tanio Manunggal");
+System.out.println("Gender : Laki Laki");
+System.out.println("Alamat : Desa Japan Rt 03 Rw 05 Dawe Kudus");
+//yang salah : System.out.perintln("Tanggal Lahir : Tuliskan Tanggal lahir Anda"); pembenaran di bawahnya
+System.out.println("Tanggal Lahir : 19 Juni 2000");
+}
+}
